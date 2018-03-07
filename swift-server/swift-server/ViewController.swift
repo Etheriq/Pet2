0f1923ae0ff03428c1aafa0bbf5d39a2bf28fdd1
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2018-03-07 23:28:52 +0200

    init

---
+++ swift-server/swift-server/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-server
+//
+//  Created by Yuriy T on 07.03.2018.
+//  Copyright © 2018 Yuriy T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
